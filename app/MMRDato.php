9fcb2e82d13a8b99229a03c13d1b24c835a3e6ff
RafaelMMGH <martinez_brg21@protonmail.com>
2018-06-28 15:57:29 -0500

    Merge

---
+++ app/MMRDato.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MMRDato extends Model
+{
+
+	protected $table = 'MMRtable'
+
+	protected $timestamp = false;
+
+	protected $guarded = ['created_at','updated_at'];
+
+    protected $fillable = ['name','apPaterno','apMaterno','phone','email','sex','age','password']; 
+}
